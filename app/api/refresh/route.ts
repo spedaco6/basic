b304f7e8d2b9dfdfbf15c04a4cdc9d9e56681daf
Spencer Conrad <spencerthedane@gmail.com>
2026-01-16 22:20:57 -0500

    Change out refresh cookie on refresh

--- app/api/refresh/route.ts
+++ app/api/refresh/route.ts
@@ -1,8 +1,7 @@
 import { FetchResponseData } from "@/hooks/useFetch";
 import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
-import { jwtVerify, SignJWT } from "jose";
-import { createAccessToken, verifyRefreshToken } from "@/lib/tokens";
+import { createAccessToken, createRefreshToken, verifyRefreshToken } from "@/lib/tokens";
 
 export interface RefreshResponseData extends FetchResponseData {
   token: string
@@ -20,6 +19,18 @@ export async function GET() {
   // Get new access token
   const payload = { userId: 1, userRole: 50 }; // todo
   const accessToken = await createAccessToken(payload);
+  const newRefreshToken = await createRefreshToken(payload); // todo set jti and save to db
+  
+  const response = NextResponse.json({ success: true, token: accessToken, message: "cookie refreshed" });
 
-  return NextResponse.json({ success: true, token: accessToken, message: "cookie refreshed" });
+  // Return response with token and cookie
+  response.cookies.set("refresh", newRefreshToken, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: 'strict',
+    path: "/",
+    maxAge: 60 * 60 * 24 * 7, // 7 days 
+  });
+
+  return response;
 }
