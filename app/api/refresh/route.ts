c354ca00fc3bdb101fd1467f27c1c0dc5e25d6c1
Spencer Conrad <spencerthedane@gmail.com>
2026-01-20 21:24:49 -0500

    Move refresh token logic into separate lib/api directory

--- app/api/refresh/route.ts
+++ app/api/refresh/route.ts
@@ -1,54 +1,26 @@
 import { FetchResponseData } from "@/hooks/useFetch";
 import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
-import { createAccessToken, createRefreshToken, verifyRefreshToken } from "@/lib/server/tokens";
-import { User } from "@/lib/server/models/User";
-import { v4 } from "uuid";
 import { HTTPError } from "@/lib/server/errors";
+import { refreshTokens } from "@/lib/api/tokens";
 
 export interface RefreshResponseData extends FetchResponseData {
   token: string
 }
 
 export async function GET() {
-  let user;
-  let verified;
   try {
     const cookieStore = await cookies();
-    const refreshToken = cookieStore.get("refresh");
-    if (!refreshToken) return NextResponse.redirect("/login");
+    const currentRefreshToken = cookieStore.get("refresh");
+    if (!currentRefreshToken) return NextResponse.redirect("/login");
+    const currentRefreshTokenValue = currentRefreshToken.value;
+
+    const { refreshToken, accessToken } = await refreshTokens(currentRefreshTokenValue);
     
-    // Validate refresh token
-    verified = await verifyRefreshToken(refreshToken.value);
-    if (!verified) throw new HTTPError("Unverified refresh token", 401);
-
-    // Check jti
-    user = await User.findById(verified.userId);
-    if (!user) throw new HTTPError("No user found", 404);
-    if (!user.jti || user.jti !== verified.jti) throw new HTTPError("Invalid refresh token", 401);
-
-    // Get new access token
-    const payload = { userId: verified.userId, userRole: verified.userRole };
-    const accessToken = await createAccessToken(payload);
-
-    // Create new jti and save to user
-    const jti = v4();
-    user.jti = jti;
-    await user.save();
-
-
-    // TESTING todo
-    const updatedUser = await User.findById(user.id);
-    if (updatedUser) console.log("user jti: ", updatedUser.jti);
-    console.log("new jti: ", jti);
-
-    
-    // Create refresh token with new jti
-    const newRefreshToken = await createRefreshToken({ ...payload, jti });
     const response = NextResponse.json({ success: true, token: accessToken, message: "Token refreshed" });
   
     // Return response with token and cookie
-    response.cookies.set("refresh", newRefreshToken, {
+    response.cookies.set("refresh", refreshToken, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
       sameSite: 'strict',
---
+++ lib/api/tokens.ts
@@ -0,0 +1,44 @@
+
+// DO NOT USE ERROR HANDLING
+// HTTP ERRORS WILL BE HANDLED IN API ROUTES
+
+import { HTTPError } from "../server/errors";
+import { User } from "../server/models/User";
+import { createAccessToken, createRefreshToken, verifyRefreshToken } from "../server/tokens";
+import { v4 } from "uuid";
+
+interface TokenPackage {
+  refreshToken: string,
+  accessToken: string,
+}
+
+export const refreshTokens = async (oldToken: string): Promise<TokenPackage> => {
+    // Validate refresh token
+    const verified = await verifyRefreshToken(oldToken);
+    if (!verified) throw new HTTPError("Unverified refresh token", 401);
+
+    // Check jti
+    const user = await User.findById(verified.userId);
+    if (!user) throw new HTTPError("No user found", 404);
+    if (!user.jti || user.jti !== verified.jti) throw new HTTPError("Invalid refresh token", 401);
+
+    // Get new access token
+    const payload = { userId: verified.userId, userRole: verified.userRole };
+    const accessToken = await createAccessToken(payload);
+
+    // Create new jti and save to user
+    const jti = v4();
+    user.jti = jti;
+    await user.save();
+
+
+    /* // TESTING todo
+    const updatedUser = await User.findById(user.id);
+    if (updatedUser) console.log("user jti: ", updatedUser.jti);
+    console.log("new jti: ", jti); */
+
+    
+    // Create refresh token with new jti
+    const newRefreshToken = await createRefreshToken({ ...payload, jti });
+    return { refreshToken: newRefreshToken, accessToken };
+}
