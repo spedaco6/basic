07f59726c68b936566b2ba62686fbf671626c22a
Spencer Conrad <spencerthedane@gmail.com>
2026-01-27 15:39:02 -0500

    Update checklist

--- app/auth/checklists/page.tsx
+++ app/auth/checklists/page.tsx
@@ -3,14 +3,15 @@ export default async function ChecklistsPage() {
 return <main className="p-4">
     <h1 className="text-2xl mb-4 uppercase">My To-Do List</h1> 
     <ul>
-      <li>Style buttons</li>
-      <li>Style Input and Select elements for disabled state</li>
-      <li>Create tests</li>
+      <s><li>Style buttons</li></s>
+      <s><li>Allow for account deletion</li></s>
+      <s><li>Reset passwords</li></s>
       <li>Allow for users to be added</li>
       <li>Simple checklist list functionality</li>
+      <li>Style Input and Select elements for disabled state</li>
+      <li>Style disabled buttons</li>
+      <li>Create tests</li>
       <li>Extract components where possible</li>
-      <li>Allow for account deletion</li>
-      <li>Reset passwords</li>
     </ul>
   </main>
 }
