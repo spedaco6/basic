09cefd36624d7d43b039bcc6bb774699e479e46f
Spencer Conrad <spencerthedane@gmail.com>
2026-02-23 18:25:06 +0100

    Profile tests

--- vitest.config.js
+++ vitest.config.js
@@ -1,6 +1,12 @@
 import { defineConfig } from 'vitest/config'
+import path from "path";
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./"),
+    },
+  },
   test: {
     setupFiles: "./vitest.setup.js",
     globals: true,
