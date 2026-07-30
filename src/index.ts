c08bc5dcfea8eb2d40bdd45c1c3413ea565faa85
Spencer Conrad <spencerthedane@gmail.com>
2026-07-30 16:49:14 +0200

    Update NPM exports

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,10 @@ import "./styles/main.css";
 
 export { useInput } from "./hooks/useInput";
 export { Input } from "./components/inputs/input/Input";
+export { Button } from "./components/inputs/button/Button";
 export { Validator } from "./lib/client/validation";
 
 export type { UseInputResult } from "./hooks/useInput";
 export type { ValidatorFn } from "./lib/client/validation";
 export type { InputProps } from "./components/inputs/input/Input";
+export type { ButtonProps } from "./components/inputs/button/Button";
