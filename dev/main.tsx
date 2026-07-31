c5b353277b7afd627ee20f69aaeb9e9417ed6754
Spencer Conrad <spencerthedane@gmail.com>
2026-07-31 10:48:02 +0200

    Update Button component so spinner spins

--- dev/main.tsx
+++ dev/main.tsx
@@ -2,18 +2,25 @@
 import ReactDOM from 'react-dom/client';
 import { Input } from "../src/components/inputs/input/Input";
 import { useInput } from '../src';
-import Button from '../src/components/inputs/button/Button';
+import { Button } from '../src/components/inputs/button/Button';
 import "../src/styles/main.css";
+import { useState } from 'react';
 
 const App = () => {
   const hook = useInput("text*", "");
+  const [loading, setLoading] = useState(false);
+  const onClick = async () => {
+    setLoading(true);
+    await new Promise(res => setTimeout(res, 2000));
+    setLoading(false);
+  }
 
   return <div className="flex flex-col gap-4 p-8 w-90">
     <Input label="Sample input" />
     <Input label="Sample text area" type="textarea" />
     <Input hook={hook} label="Sample text area" type="select" required allowEmpty options={["this", "that", "another"]}/>
     <Input label="This should not be uppercase" type="checkbox" />
-    <Button showLoadingSpinner>Test</Button>
+    <Button showLoading loading={loading} onClick={onClick}>Test</Button>
   </div>
 };
 
