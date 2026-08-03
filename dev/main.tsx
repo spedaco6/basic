db50ce61d504f4eb43143f2ea9a0ecd5f34591cf
Spencer Conrad <spencerthedane@gmail.com>
2026-08-03 22:10:20 +0200

    Revert main back

--- dev/main.tsx
+++ dev/main.tsx
@@ -1,31 +1,19 @@
 "use client"
 import ReactDOM from 'react-dom/client';
-import { useInput } from '../src';
-import { useState } from 'react';
-import { Form } from '../src/components/form/Form';
+import { Button, Input } from '../src';
 import "../src/styles/main.css";
 
 const App = () => {
-  const [loading, setLoading] = useState(false);
-  const onClick = async () => {
-    setLoading(true);
-    await new Promise(res => setTimeout(res, 2000));
-    setLoading(false);
-  }
 
-  return <Form url=""inputs={{
-    one: useInput("one*", ""),
-    two: useInput("two", ''),
-    three: useInput("three*", 'another'),
-    four: useInput("four", false),
-  }} className="flex flex-col gap-4 p-8 w-90" loading={loading}>
-    <Form.Input name="one" label="Sample input" type="password" allowShow />
-    <Form.Input name="two" label="Sample text area" type="textarea" />
-    <Form.Input name="three" label="Sample text area" type="select" required allowEmpty options={["this", "that", "another"]}/>
-    <Form.Input name="four" label="This should not be uppercase" type="checkbox" />
-    <Form.Button action="cancel" onClick={(data) => console.log(data)}>Cancel</Form.Button>
-    <Form.Button action="submit" onClick={onClick}>Submit</Form.Button>
-  </Form>
+
+  return <form className="flex flex-col gap-4 p-8 w-90">
+    <Input name="one" label="Sample input" type="password" allowShow />
+    <Input name="two" label="Sample text area" type="textarea" />
+    <Input name="three" label="Sample text area" type="select" required allowEmpty options={["this", "that", "another"]}/>
+    <Input name="four" label="This should not be uppercase" type="checkbox" />
+    <Button>Cancel</Button>
+    <Button>Submit</Button>
+  </form>
 };
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
